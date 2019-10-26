e114e73d74789b4f245d9bf6c0c5493ca16637e7
thalles <thalles.freitas@zup.com.br>
2019-10-26 16:01:25 -0300

    "Correções desafio 3"

--- gg-fase-1-master/request/src/main/java/com/br/zup/juniors/request/Service.java
+++ gg-fase-1-master/request/src/main/java/com/br/zup/juniors/request/Service.java
@@ -12,39 +12,60 @@ import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Service {
 
-	public static void enviaPost() {
-		// TODO Auto-generated method stub
-    	System.out.println("Requisição post");
-    	Map<String,Object> mapDados = new HashMap<String,Object>();
-    	mapDados.put("id",123);
-    	mapDados.put("field","testing");
-        ObjectMapper objectMapper = new ObjectMapper();
-        String requestBody = null;
-		try {
-			requestBody = objectMapper
-			      .writerWithDefaultPrettyPrinter()
-			      .writeValueAsString(mapDados);
-		} catch (JsonProcessingException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
+	public static  HttpRequest criaRequest(String url,String tipo,StringBuilder dados) {
+		HttpRequest request = null;
+		if(tipo.equals("POST")) {
 
-        HttpClient client = HttpClient.newHttpClient();
-        try {
-			HttpRequest request = HttpRequest.newBuilder()
-					.uri(URI.create("http://httpbin.org/post")).
+			request = HttpRequest.newBuilder()
+					.uri(URI.create(url)).
 					timeout(Duration.ofMinutes(1))
 					.header("Content-Type","Application/json").
-					POST(BodyPublishers.ofString(requestBody)).build();
-					
-				HttpResponse<String> response = client.send(request,BodyHandlers.ofString());
-				System.out.println(response.body());
+					POST(BodyPublishers.ofString(dados.toString())).build();
+		}
+		if(tipo.equals("DELETE")) {
+			request = HttpRequest.newBuilder()
+					.uri(URI.create(url)).
+					timeout(Duration.ofMinutes(1))
+					.header("Content-Type","Application/json").
+					DELETE().build();
+
+		}
+		if(tipo.equals("BEARER")) {
+			request = HttpRequest.
+					newBuilder().uri(URI.create(url))
+					.timeout(Duration.ofMinutes(1))
+					.GET()
+					.header("Content-Type","application/json")
+					.header("Authorization","Bearer "+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
+					.build();
+		}
+		return request;
+	}
+	public static StringBuilder transformaMapString() {
+		Map<String,Object> mapDados = new HashMap<String,Object>();
+		mapDados.put("id",123);
+		mapDados.put("field","testing");
+		StringBuilder mapAsString = new StringBuilder("{");
+		for (String key : mapDados.keySet()) {
+			mapAsString.append(key + ":" + mapDados.get(key) + ",");
+		}
+		mapAsString.delete(mapAsString.length()-1, mapAsString.length()).append("}");
+		return mapAsString;
+	}
+	public static void enviaPost() {
+		// TODO Auto-generated method stub
+		HttpRequest request = null;
+		System.out.println("Requisição post");
+		StringBuilder mapAsString = Service.transformaMapString();
+		request = 	Service.criaRequest("http://httpbin.org/post","POST",mapAsString);
+		HttpClient client = HttpClient.newHttpClient();
+		try {
+
+			HttpResponse<String> response = client.send(request,BodyHandlers.ofString());
+			System.out.println(response.body());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -58,41 +79,29 @@ public class Service {
 	}
 	public static void enviaDelete() {
 		// TODO Auto-generated method stub
-    	
-        
+
+
 		System.out.println("Requisição delete");
-        HttpClient client = HttpClient.newHttpClient();
-        try {
-			HttpRequest request = HttpRequest.newBuilder()
-					.uri(URI.create("http://httpbin.org/delete")).
-					timeout(Duration.ofMinutes(1))
-					.header("Content-Type","Application/json").
-					DELETE().build();
-					
-				HttpResponse<String> response = client.send(request,BodyHandlers.ofString());
-				System.out.println(response.body());
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		HttpClient client = HttpClient.newHttpClient();
+			HttpRequest request = Service.criaRequest("http://httpbin.org/delete","DELETE",null);
+
+			HttpResponse<String> response = null;
+			try {
+				response = client.send(request,BodyHandlers.ofString());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(response.body());
 	}
 	public static void enviaBearer() {
 		System.out.println("Requisição bearer");
 		// TODO Auto-generated method stub
 		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.
-				newBuilder().uri(URI.create("http://httpbin.org/bearer"))
-				.timeout(Duration.ofMinutes(1))
-				.GET()
-				.header("Content-Type","application/json")
-				.header("Authorization","Bearer "+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
-				.build();
+		HttpRequest request = Service.criaRequest("http://httpbin.org/bearer","BEARER",null);
 		try {
 			HttpResponse<String> response = client.send(request,BodyHandlers.ofString());
 			System.out.println("Resposta: "+response.body());
