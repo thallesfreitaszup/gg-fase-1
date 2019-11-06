f2bd2b0032d04773c31d3d9d953ec56d0b0e5823
thalles <thalles.freitas@zup.com.br>
2019-11-06 11:26:27 -0300

    Teste unitario

--- gg-fase-1-master/urlLeitura/src/test/java/com/br/zup/juniors/urlLeitura/ServiceTest.java
+++ gg-fase-1-master/urlLeitura/src/test/java/com/br/zup/juniors/urlLeitura/ServiceTest.java
@@ -1,38 +1,55 @@
 package com.br.zup.juniors.urlLeitura;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
 public class ServiceTest 
-    
+
 {
 	List<String> lista = null;
+	StringBuilder titulos = new StringBuilder();
+	String linha  = null;
+	List<String> listaString =  new ArrayList<String>(); 
 	@BeforeEach
 	public void setup() throws IOException, InterruptedException {
+		System.out.println("request");
 		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder().
-		uri(URI.create("https://www.reddit.com/r/programming/")).build();
-		 lista = Arrays.asList(client.send(request,BodyHandlers.ofString()).body().split(","));
-		 
-		 
+		BufferedReader bf = new BufferedReader(new FileReader("teste.txt"));
+		while(( linha = bf.readLine())!= null) {
+			titulos.append(linha);
+		}
+		listaString.add("Using spring boot");
+		listaString.add("How to make a test file");
+		
 	}
-   @Test
-   public void pegaTitulosItensTest(){
-	   assertEquals("",Service.pegaTitulosItens(lista));
-   }
+	//Teste que verifica se os 20 itens foram armazenados
+	@Test
+	public void pegaTitulosItensTest(){
+		
+		assertEquals("[Using spring boot, How to make a test file]",Service.pegaTitulosItens(Arrays.asList(titulos.toString().split(","))).toString());
+	}
+	@Test
+	public void ordenaTitulosItensTest(){
+		Collections.sort(listaString);
+		assertEquals(listaString,Service.ordenaTitulos(listaString));
+	}
 }
