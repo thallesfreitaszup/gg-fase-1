f3234ce881b36d35b8c4f9ce4785a4f7a0c25f01
thalles <thalles.freitas@zup.com.br>
2019-11-13 16:40:19 -0300

    correções desafio 2

--- gg-fase-1-master/urlLeitura/src/test/java/com/br/zup/juniors/urlLeitura/ServiceTest.java
+++ gg-fase-1-master/urlLeitura/src/test/java/com/br/zup/juniors/urlLeitura/ServiceTest.java
@@ -3,20 +3,13 @@ package com.br.zup.juniors.urlLeitura;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -25,25 +18,25 @@ import org.junit.jupiter.api.Test;
 public class ServiceTest 
 
 {
-	List<String> lista = null;
-	StringBuilder titulos = new StringBuilder();
-	String linha  = null;
-	List<String> listaString =  new ArrayList<String>(); 
-	@BeforeEach
-	public void setup() throws IOException, InterruptedException {
-		System.out.println("request");
-		HttpClient client = HttpClient.newHttpClient();
-		BufferedReader bf = new BufferedReader(new FileReader("teste.txt"));
-		while(( linha = bf.readLine())!= null) {
-			titulos.append(linha);
-		}
-		listaString.add("Using spring boot");
-		listaString.add("How to make a test file");
-		
-	}
+
+	
 	//Teste que verifica se os 20 itens foram armazenados
 	@Test
 	public void pegaTitulosItensTest(){
+		StringBuilder titulos = new StringBuilder();
+		String linha  = null;
+		try(BufferedReader bf = new BufferedReader(new FileReader("teste.txt"))){			
+			while(( linha = bf.readLine())!= null) {
+				titulos.append(linha);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			System.out.println(e1.getMessage());
+		}
+			
 		
 		assertEquals("[Using spring boot, How to make a test file]",Service.pegaTitulosItens(Arrays.asList(titulos.toString().split(","))).toString());
 	}
@@ -51,7 +44,14 @@ public class ServiceTest
 	@Test
 	
 	public void ordenaTitulosItensTest(){
-		Collections.sort(listaString);
-		assertEquals(listaString,Service.ordenaTitulos(listaString));
+		List<String> listaString =  new ArrayList<String>(); 
+		List<String> listaCompara =  new ArrayList<String>(); 
+		listaString.add("Using spring boot");
+		listaString.add("How to make a test file");
+		listaCompara.add("How to make a test file");
+		listaCompara.add("Using spring boot");
+		assertEquals(listaCompara,Service.ordenaTitulos(listaString));
 	}
+	
+	
 }
